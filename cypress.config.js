9c6ffddb3cc1cf7f3407b1e5ae9720d26ddf657b
OrivanJunior <orivan.junior@viasoft.com.br>
2024-02-27 10:27:34 -0300

    alterações gerais, tentando configurar CI

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const cucumber = require('cypress-cucumber-preprocessor').default
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'uqjmju',
   e2e: {
     setupNodeEvents(on, config) {
       on('file:preprocessor', cucumber())
--- cypress/support/Scripts/SC - LoginDois/Actions/LoginDoisAction.js
+++ cypress/support/Scripts/SC - LoginDois/Actions/LoginDoisAction.js
@@ -7,21 +7,21 @@ export class LoginDoisAction {
     ExistCampoNome(){
         cy.get(LoginDoisElement.LoginPageLocators.name)
         .should('be.visible')
-        return
+
     }
     PreencheNome(PreencheNome){
         cy.get(LoginDoisElement.LoginPageLocators.name)
         .type(PreencheNome)
-        return
+
     }
     PreencheFone(PreencheFone){
         cy.get(LoginDoisElement.LoginPageLocators.telefone)
         .type(PreencheFone)
-        return
+
     }
     ClickBtnCadastrar(){
         cy.get(LoginDoisElement.LoginPageLocators.BtnCadastrar)
         .click()
-        return
+
     }
 }
